724adb73f964c0ee4d8a178cf82e4ed47d5980d0
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-06-13 03:39:28 +0300

    test

--- linked-list/tests/test_linked_list.py
+++ linked-list/tests/test_linked_list.py
@@ -1,5 +1,15 @@
 from linked_list import __version__
+from linked_list.linked_list import (
+    Node,
+    LinkedList,   
+)
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_append_1():
+    ll = LinkedList()
+    ll.append(1)
+    assert ll.head.value == 1
