72a3f89a9497fc8cc3a1b2ad3f0422f27eab0a39
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-07-26 12:23:01 +0300

    fin

--- challenges/repeated_word/tests/test_repeated_word.py
+++ challenges/repeated_word/tests/test_repeated_word.py
@@ -1,9 +1,13 @@
 from repeated_word import __version__
+from repeated_word.repeated_word import word_repeated
 
 
 def test_version():
     assert __version__ == '0.1.0'
 
 
-def test():
-    pass
+def test_repated_word():
+    the_string = 'my name is hadi nofal , hadi is software engineer , is'
+
+    result = word_repeated(the_string)
+    assert result == 'is'
